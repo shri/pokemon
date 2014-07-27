6726605d3e36211cd380556bc9cf2beec6b5cf8d
dmlittle <dmlittle@mit.edu>
2014-07-26 17:17:01 -0700

    added pokemon db model

---
+++ server/pokemons.js
@@ -0,0 +1,72 @@
+Pokemons = new Meteor.Collection('pokemons');
+
+Pokemons.allow({
+  insert: function (userId, pokemon, fields, modifier) {
+      return false;
+  },
+  update: function (userId, pokemon, fields, modifier) {
+      return true;
+  },
+  remove: function (userId, pokemon) {
+      // Only pokemon trainers can delete their pokemon
+      if (userId == pokemon.trainer) {
+          return true;
+      }
+    return false;
+  }
+
+});
+
+Meteor.methods({
+    catchPokemon: function(pokemon) {
+        if (! this.userId)
+            throw new Meteor.Error(403, "You must be logged in");
+        if (pokemon.pokemon < 1 || pokemon.pokemon > 151 )
+            throw new Meteor.Error(400, "Unsupported Pokemon");
+        if (! pokemon.trainer )
+            throw new Meteor.Error(400, "You must specify a pokemon trainer");
+        if (!(pokemon.level > 0 && pokemon.level <= 100))
+            throw new Meteor.Error(400, "Pokemon level must range between 1-100");
+        if (!pokemon.attack || !pokemon.spattack || !pokemon.defense || !pokemon.spdefense)
+            throw new Meteor.Error(400, "Pokemon must have all stats");
+        if (!pokemon.HP || !pokemon.remainingHP)
+            throw new Meteor.Error(400, "Pokemon must have HP");
+        if (!pokemon.exp)
+            throw new Meteor.Error(400, "Pokemon must have exp");
+            
+        return Pokemons.insert({
+            trainer: this.userId,
+            pokemon: pokemon.pokemon,
+            level: pokemon.level,
+            moves: pokemon.moves,
+            attack: pokemon.attack,
+            spattack: pokemon.spattack,
+            defense: pokemon.defense,
+            spdefense: pokemon.spdefense,
+            HP: pokemon.HP,
+            remainingHP:pokemon.remainingHP,
+            exp: pokemon.exp,
+            status: pokemon.status
+        });
+    },
+    healPokemon: function(pokemon) {
+        if (pokemon.trainer != this.userId)
+            throw new Meteor.Error(400, "You can only heal your own pokemons");
+        return Pokemons.update({ _id: pokemon._id }, { $set: { "remainingHP": pokemon.HP }});
+    },
+    releasePokemon: function(pokemon) {
+        if (pokemon.trainer == this.userId)
+            return Pokemons.remote({ _id: pokemon._id });
+    },
+    addMoveToPokemon: function(pokemon, move) {
+        if (pokemon.moves >= 4)
+            throw new Meteor.Error(400, "Pokemon has max number of moves");
+        return Pokemons.update({ _id: pokemon._id }, { $addToSet: { moves: move } });
+    },
+    removeMoveFromPokemon: function(pokemon, move) {
+        return Pokemons.update({ _id: pokemon._id }, { $pull : { moves: move } });
+    },
+    updatePokemonStatus: function(pokemon, status) {
+        return Pokemons.update({ _id: pokemon._id }, { $set : { status: status } });
+    }
+});
