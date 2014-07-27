681b96aa1946d9697aa39b6fb9294ce9586eacbb
connie <connie@flux.io>
2014-07-27 07:50:39 -0700

    added grass

--- client/js/3_entities.js
+++ client/js/3_entities.js
@@ -7,12 +7,11 @@ game.PlayerEntity = me.ObjectEntity.extend({
         this.parent(x, y, settings);
 
         // set the default horizontal & vertical speed (accel vector)
-        this.setVelocity(16, 16);
+        this.setVelocity(4, 4);
 
         // set the display to follow our position on both axis
         me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
 
-        this.collidable = true;
         this.gravity = 0;
 
         this.speed = 4;
@@ -22,31 +21,26 @@ game.PlayerEntity = me.ObjectEntity.extend({
 
     /* -----
 
-		update the player pos
-		
-	  ------			*/
+        update the player pos
+        
+      ------            */
     update: function(dt) {
 
         if (me.input.isKeyPressed('left')) {
-            // flip the sprite on horizontal axis
-            // this.flipX(true);
-            // update the entity velocity
+            this.flipX(true);
             this.vel.x -= this.speed * 1;
+            isInGrass = false;
+
         } else if (me.input.isKeyPressed('right')) {
-            // unflip the sprite
-            // this.flipX(false);
-            // update the entity velocity
-            this.vel.x += this.speed * 1 ;
+            this.flipX(false);
+            this.vel.x += this.speed * 1;
+            isInGrass = false;
         } else if (me.input.isKeyPressed('up')) {
-            // unflip the sprite
-            // this.flipX(false);
-            // update the entity velocity
-            this.vel.y -= this.speed * 1 ;
+            this.vel.y -= this.speed * 1;
+            isInGrass = false;
         } else if (me.input.isKeyPressed('down')) {
-            // unflip the sprite
-            // this.flipX(false);
-            // update the entity velocity
-            this.vel.y += this.speed * 1 ;
+            this.vel.y += this.speed * 1;
+            isInGrass = false;
         } else {
             this.vel.x = 0;
             this.vel.y = 0;
@@ -58,20 +52,11 @@ game.PlayerEntity = me.ObjectEntity.extend({
         // check for collision
         var res = me.game.world.collide(this);
 
+
         if (res) {
             if (res.obj.type == me.game.ENEMY_OBJECT) {
-                if ((res.y > 0) && !this.jumping) {
-                    // bounce (force jump)
-                    this.falling = false;
-                    this.vel.y = -this.maxVel.y * me.timer.tick;
-                    // set the jumping flag
-                    this.jumping = true;
-                    // play some audio
-                    me.audio.play("stomp");
-                } else {
                     // let's flicker in case we touched an enemy
-                    this.renderable.flicker(750);
-                }
+                    // this.renderable.flicker(750);
             }
         }
 
@@ -88,4 +73,33 @@ game.PlayerEntity = me.ObjectEntity.extend({
         return false;
     }
 
+});
+
+
+game.EnemyEntity = me.ObjectEntity.extend({
+    init: function(x, y, settings) {
+        // define this here instead of tiled
+        settings.image = "";
+
+        // save the area size defined in Tiled
+        var width = settings.width;
+        var height = settings.height;
+
+        // call the parent constructor
+        this.parent(x, y, settings);
+
+        // make it collidable
+        this.collidable = true;
+        this.type = me.game.ENEMY_OBJECT;
+
+    },
+
+
+    onCollision: function(res, obj) {
+        console.log('grass');
+        if (Math.random(1) < 0.001) {
+            alert('pokemon battle!');
+            me.game.world.removeChild(this);
+        }
+    }
 });
