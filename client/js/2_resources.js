51dbc10d7f52a90f6db7ae03d21d082e3038d9a6
connie <connie@flux.io>
2014-07-27 02:37:00 -0700

    added house inside

--- client/js/2_resources.js
+++ client/js/2_resources.js
@@ -1,43 +1,83 @@
 game.resources = [
-	/**
-	 * Graphics.
-	 */
-	// our level tileset
-	{name: "area01_level_tiles",  type:"image",	src: "data/img/map/area01_level_tiles.png"},
-	{name: "normal1",  type:"image",	src: "data/img/map/normal1.png"},
-	{name: "normal2",  type:"image",	src: "data/img/map/normal2.png"},
-	{name: "metatiles16x16",  type:"image", src: "data/img/map/metatiles16x16.png"},
+    /**
+     * Graphics.
+     */
+    // our level tileset
+    {
+        name: "area01_level_tiles",
+        type: "image",
+        src: "data/img/map/area01_level_tiles.png"
+    }, {
+        name: "normal1",
+        type: "image",
+        src: "data/img/map/normal1.png"
+    }, {
+        name: "normal2",
+        type: "image",
+        src: "data/img/map/normal2.png"
+    }, {
+        name: "metatiles16x16",
+        type: "image",
+        src: "data/img/map/metatiles16x16.png"
+    },
+    {
+        name: "inside_house",
+        type: "image",
+        src: "data/img/map/inside_house.png"
+    },
 
-	// the main player spritesheet
-	{name: "gripe_run_right",     type:"image",	src: "data/img/sprite/gripe_run_right.png"},
-	// the parallax background
-	{name: "area01_bkg0",         type:"image",	src: "data/img/area01_bkg0.png"},
-	{name: "area01_bkg1",         type:"image",	src: "data/img/area01_bkg1.png"},
-	// the spinning coin spritesheet
-	{name: "spinning_coin_gold",  type:"image",	src: "data/img/sprite/spinning_coin_gold.png"},
-	// our enemty entity
-	{name: "wheelie_right",       type:"image",	src: "data/img/sprite/wheelie_right.png"},
-	// game font
-	{name: "32x32_font",          type:"image",	src: "data/img/font/32x32_font.png"},
-	
-	/* 
-	 * Maps. 
- 	 */
-	{name: "area02",              type: "tmx",	src: "data/map/area02.tmx"},
-	{name: "area03",              type: "tmx",	src: "data/map/area03.tmx"},
-	{name: "area04",              type: "tmx",	src: "data/map/map2.tmx"},
+    // the main player spritesheet
+    {
+        name: "gripe_run_right",
+        type: "image",
+        src: "data/img/sprite/gripe_run_right.png"
+    },
+
+    // our enemty entity
+    {
+        name: "wheelie_right",
+        type: "image",
+        src: "data/img/sprite/wheelie_right.png"
+    },
+    // game font
+    {
+        name: "32x32_font",
+        type: "image",
+        src: "data/img/font/32x32_font.png"
+    },
+
+    /* 
+     * Maps.
+     */
+    {
+        name: "area02",
+        type: "tmx",
+        src: "data/map/area02.tmx"
+    }, {
+        name: "area03",
+        type: "tmx",
+        src: "data/map/area03.tmx"
+    }, {
+        name: "area04",
+        type: "tmx",
+        src: "data/map/map2.tmx"
+    },{
+        name: "inside_house",
+        type: "tmx",
+        src: "data/map/inside_house.tmx"
+    },
 
 
-	// /* 
-	//  * Background music. 
-	//  */	
-	// {name: "dst-inertexponent", type: "audio", src: "data/bgm/"},
-	
-	// /* 
-	//  * Sound effects. 
-	//  */
-	// {name: "cling", type: "audio", src: "data/sfx/"},
-	// {name: "stomp", type: "audio", src: "data/sfx/"},
-	// {name: "jump",  type: "audio", src: "data/sfx/"}
+    // /* 
+    //  * Background music. 
+    //  */  
+    // {name: "dst-inertexponent", type: "audio", src: "data/bgm/"},
+
+    // /* 
+    //  * Sound effects. 
+    //  */
+    // {name: "cling", type: "audio", src: "data/sfx/"},
+    // {name: "stomp", type: "audio", src: "data/sfx/"},
+    // {name: "jump",  type: "audio", src: "data/sfx/"}
 
 ];
